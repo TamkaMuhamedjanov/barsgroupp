ebd86dae9272f3fc4525d6c58bbc501b957ddeb6
TamkaMuhamedjanov <tima.muhamejanov@yandex.ru>
2022-04-04 17:20:14 +0300

    ex1

---
+++ Loger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    internal interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception ex);
+    }
+     public class Loger<T> : ILogger
+    {
+        
+        private readonly string path;
+        private string GenericTypeName => typeof(T).Name;
+
+        public Loger(string path)
+        {
+            this.path = path;
+        }
+
+        public void LogInfo(string message)
+        {
+            using StreamWriter fs = new StreamWriter(path, true); 
+            fs.WriteLine($"[Info]: [{typeof(T).Name}] : {message}");
+        }
+        public void LogWarning(string message)
+        {
+            using StreamWriter fs = new StreamWriter(path, true);
+            fs.WriteLine($"[Warning]: [{typeof(T).Name}] : {message}");
+        }
+        public void LogError(string message, Exception ex)
+        {
+            using StreamWriter fs = new StreamWriter(path, true);
+            fs.WriteLine($"[Error] : [{typeof(T).Name}] : {message}. {ex.Message}");
+        }
+               
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var LogString = new Loger<string>("LogString.txt");
+            LogString.LogInfo("Животный мир");
+            LogString.LogWarning("Предупреждение");
+            LogString.LogError("Oшибка", new ArgumentNullException());
+
+            var LogAnimal = new Loger<Animal>("LogAnimal.txt");
+            LogAnimal.LogInfo("Животный мир");
+            LogAnimal.LogWarning("Предупреждение");
+            LogAnimal.LogError("Oшибка", new NullReferenceException());
+        }
+    }
+
+}
